fb6437c4f7444e0246431abea3dc40b42aa6db50
Jesse Cogollo <cogollo87@gmail.com>
2016-04-08 00:55:25 -0500

    response static files with express

--- server/routes.js
+++ server/routes.js
@@ -41,7 +41,7 @@ module.exports = function (app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function (req, res) {
-      res.sendfile(app.get('appPath') + '/index.html')
+      res.status(200).sendFile(app.get('appPath') + '/index.html')
     })
 
   app.use(pmx.expressErrorHandler())
