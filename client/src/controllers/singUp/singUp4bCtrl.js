34af9c5ccf8fd0391a19d67aed79e37d3f63d8bd
Jesse Cogollo <cogollo87@gmail.com>
2016-05-02 11:03:56 -0500

    mask ABA Done.

--- client/src/controllers/singUp/singUp4bCtrl.js
+++ client/src/controllers/singUp/singUp4bCtrl.js
@@ -1,7 +1,7 @@
 'use strict'
 var angular = require('angular')
 
-module.exports = [ '$scope', 'SingUpService', '$state', 'UserService', function ($scope, SingUpService, $state, UserService) {
+module.exports = ['$scope', 'SingUpService', '$state', 'UserService', function ($scope, SingUpService, $state, UserService) {
   $scope.next = function () {
     // Validation start
     var f = $scope.form
@@ -36,6 +36,56 @@ module.exports = [ '$scope', 'SingUpService', '$state', 'UserService', function 
       f.uDDA2.$setValidity('match', false)
     } else {
       f.uDDA2.$setValidity('match', true)
+    }
+  }
+  $scope.user = {}
+  $scope.bankAccount = {}
+  $scope.maskABA = function () {
+    if ($scope.user.routingNumber) {
+      $scope.bankAccount.routingNumber = angular.copy($scope.user.routingNumber)
+      var temp = ''
+      for (var i = 0; i < $scope.user.routingNumber.length; i++) {
+        temp += '*'
+      }
+      $scope.user.routingNumber = temp
+    }
+  }
+
+  $scope.unmaskABA = function () {
+    // console.log('unmaskABA temp', temp)
+    if ($scope.bankAccount.routingNumber) {
+      $scope.user.routingNumber = angular.copy($scope.bankAccount.routingNumber)
+    }
+  }
+
+  $scope.ABAValidator = function () {
+    // Taken from: http://www.brainjar.com/js/validation/
+    $scope.bankAccount.routingNumber = angular.copy($scope.user.routingNumber)
+    if (!$scope.bankAccount.routingNumber) {
+      $scope.form.uRoutingNumber.$setValidity('aba', false)
+      return
+    } else {
+      $scope.bankAccount.routingNumber = $scope.bankAccount.routingNumber.replace(/ /g, '')
+      if ($scope.bankAccount.routingNumber.length !== 9) {
+        $scope.form.uRoutingNumber.$setValidity('aba', false)
+        return
+      }
+    }
+    var t = $scope.bankAccount.routingNumber.replace(/ /g, '')
+    var n = 0
+    for (var i = 0; i < t.length; i += 3) {
+      n += parseInt(t.charAt(i), 10) * 3 +
+        parseInt(t.charAt(i + 1), 10) * 7 +
+        parseInt(t.charAt(i + 2), 10)
+    }
+
+    // If the resulting sum is an even multiple of ten (but not zero),
+    // the aba routing number is good.
+
+    if (n !== 0 && n % 10 === 0) {
+      $scope.form.uRoutingNumber.$setValidity('aba', true)
+    } else {
+      $scope.form.uRoutingNumber.$setValidity('aba', false)
     }
   }
 }]
