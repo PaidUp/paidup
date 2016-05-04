e461c8b612facf35981539a361e5b8d952778488
Diego Fortes <daigof@gmail.com>
2016-05-04 18:39:31 -0300

    edit create and delete account boxes and fixes

--- client/src/controllers/dashboard/accountsMenuCtrl.js
+++ client/src/controllers/dashboard/accountsMenuCtrl.js
@@ -3,9 +3,13 @@ var angular = require('angular')
 
 module.exports = [ '$scope', 'UserService', '$timeout', '$rootScope', function ($scope, UserService, $timeout, $rootScope) {
   $scope.loading = false
-  $scope.editAccount = false
   $scope.loader = '<i class="fa fa-circle-o-notch fa-spin"></i>'
   $scope.states = UserService.getStates()
+  $scope.modalAccount = {}
+  $scope.showAccountModal = false
+  $scope.editingAccount = false
+  $scope.deletingAccount = false
+  $scope.isNewCard = false
 
   var billingAddress = {
     street: '123 Main St.',
@@ -41,17 +45,43 @@ module.exports = [ '$scope', 'UserService', '$timeout', '$rootScope', function (
     $scope.activeAccountMenu = true
   })
 
-  $scope.modalAccount = {}
-  $scope.showAccountModal = false
   $scope.showAccount = function (account) {
-    console.log(account)
     $scope.modalAccount = angular.copy(account)
+    $scope.editingAccount = false
+    $scope.deletingAccount = false
+    $scope.isNewCard = false
+    $scope.accountModalTitle = 'Account Details'
     $scope.showAccountModal = true
   }
 
   $scope.closeModal = function () {
-    $scope.activeAccountMenu = true
     $scope.showAccountModal = false
+  }
+
+  $scope.linkNewCard = function () {
+    $scope.modalAccount = {}
+    $scope.accountModalTitle = 'Link New Account'
+    $scope.isNewCard = true
+    $scope.editingAccount = true
+    $scope.showAccountModal = true
+  }
+
+  $scope.editAccount = function () {
+    $scope.accountModalTitle = 'Edit Account Details'
+    $scope.isNewCard = false
+    $scope.editingAccount = true
+  }
+
+  $scope.deleteAccount = function () {
+    $scope.accountModalTitle = 'Are you sure you want to delete this Account'
+    $scope.deletingAccount = true
+  }
+
+  $scope.cancelEditing = function () {
+    $scope.editingAccount = false
+    if ($scope.isNewCard) {
+      $scope.showAccountModal = false
+    }
   }
 
   $scope.selectAccount = function (accountId) {
