81246a929045bc62b4bbefdbd481febc71be0f08
Diego Fortes <daigof@gmail.com>
2016-05-06 12:53:18 -0300

    remove cmoment

--- client/src/controllers/signUp/signUpCtrl.js
+++ client/src/controllers/signUp/signUpCtrl.js
@@ -3,6 +3,6 @@
 module.exports = [ '$scope', 'SignUpService', '$state', function ($scope, SignUpService, $state) {
   $scope.PageOptions.pageClass = 'signup-page'
   if (!$state.is('signup.step0') && !SignUpService.isValidSession()) {
-    // $state.go('login')
+    $state.go('login')
   }
 }]
