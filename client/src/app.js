a07e6d7d7034bf011481696b3f51ccaf82ed3a12
Diego Fortes <daigof@gmail.com>
2016-05-11 18:34:21 -0300

    alert System

--- client/src/app.js
+++ client/src/app.js
@@ -7,6 +7,7 @@ require('angular-sanitize')
 require('angular-cookies')
 require('angular-translate')
 require('./vendor/angular-facebook')
+require('./vendor/alert.min')
 require('angulartics')
 require('angulartics-google-analytics')
 require('angular-ui-mask')
@@ -20,7 +21,8 @@ var paidUpApp = angular.module('paidUpApp', [
   'facebook',
   'angulartics',
   'angulartics.google.analytics',
-  'ui.mask' ])
+  'ui.mask',
+  'ui.bootstrap' ])
 
 paidUpApp.config(require('./appConfig'))
 paidUpApp.run(require('./appRun'))
