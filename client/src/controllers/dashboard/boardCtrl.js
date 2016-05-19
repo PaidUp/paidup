c741058fc16a17567a169da8dd00215d0e92551e
Jesse Cogollo <cogollo87@gmail.com>
2016-05-19 17:11:48 -0500

    create report dinamically

--- client/src/controllers/dashboard/boardCtrl.js
+++ client/src/controllers/dashboard/boardCtrl.js
@@ -1,18 +1,44 @@
 'use strict'
 
 module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', function ($scope, AuthService, $state, CommerceService) {
-  $scope.expandSection1 = true
-  $scope.expandSection2 = true
-  // $scope.allOrdersGroupByProductId = [{'14U': [{o: 1}, {0: 2}]}]
-  $scope.allOrders = []
-  $scope.allOrdersGroupedByProductId = {
+  $scope.expandCategory1 = true
+  $scope.expandSection11 = true
+  $scope.groupProducts = {
     '14U': {
       productName: '14U',
       total: 2000,
       discount: 0,
       paid: 1000,
       remaining: 1000,
-      orders: [1, 2, 3, 4]
+      orders: [{
+        createAt: '2016-05-03T06:02:12.692Z',
+        productName: 'NY Yankess',
+        beneficiaryName: 'Edgar Renteria',
+        total: '1000',
+        discount: '0',
+        paid: '1000',
+        remaining: '0',
+        orderId: '0001',
+        status: '0001',
+        pp: [{
+          dateCharge: '2016-05-03T06:02:12.692Z',
+          description: 'description',
+          brand: 'visa',
+          last4: '1234',
+          originalPrice: 950,
+          price: 1000,
+          status: 'pending'
+        },
+        {
+          dateCharge: '2016-05-03T06:02:12.692Z',
+          description: 'ok',
+          brand: 'visa',
+          last4: '1234',
+          originalPrice: 950,
+          price: 1000,
+          status: 'failed'
+        }]
+      }]
     },
     '15U': {
       productName: '15U',
@@ -20,7 +46,7 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
       discount: 0,
       paid: 2000,
       remaining: 2000,
-      orders: [1, 2, 3]
+      orders: []
     },
     '16U': {
       productName: '16U',
@@ -28,7 +54,7 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
       discount: 0,
       paid: 1500,
       remaining: 500,
-      orders: [1]
+      orders: []
     },
     '17U': {
       productName: '17U',
@@ -36,106 +62,25 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
       discount: 0,
       paid: 1000,
       remaining: 1000,
-      orders: [1, 2]
+      orders: []
     }
   }
   AuthService.getCurrentUserPromise().then(function (user) {
-    console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
-    CommerceService.orderGetOrganization('acct_18AQWDGKajSrnujf', 200, -1).then(function (result) {
-      console.log('result', result.body)
-      $scope.allOrders = result.body
-      result.body.map(function (order) {
-        console.log('order', order)
-        return order
+    CommerceService.orderGetOrganization(user._id, 20, -1).then(function (result) {
+      console.log('result', result.body.orders)
+      // $scope.allOrdersBoard = result.body.orders
+      console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
+      CommerceService.orderGetOrganization('acct_18AQWDGKajSrnujf', 200, -1).then(function (result) {
+        console.log('result', result.body)
+        result.body.map(function (order) {
+          console.log('order', order)
+          return order
+        })
+      }).catch(function (err) {
+        console.log('err', err)
       })
-      // $scope.totalPrice = getTotalPrice(result.body)
-      // $scope.totalDiscount = getTotalDiscount(result.body)
-      // $scope.totalPaid = getTotalPaid(result.body)
-      // $scope.totalRemaining = getTotalRemaining(result.body)
     }).catch(function (err) {
       console.log('err', err)
     })
-  }).catch(function (err) {
-    console.log('err', err)
   })
-
-  function getTotalPrice (orders) {
-    return orders.reduce(function (previousPrice, order) {
-      return previousPrice + order.paymentsPlan.price
-    }, 0) || 0
-  }
-
-  function getTotalDiscount (orders) {
-    return orders.reduce(function (previousPrice, order) {
-      return previousPrice + order.discount
-    }, 0) || 0
-  }
-
-  function getTotalPaid (orders) {
-    return orders.filter(function (order) {
-      return order.paymentsPlan.status === 'succeeded'
-    }).reduce(function (previousPrice, order) {
-      return previousPrice + order.paymentsPlan.price
-    }, 0)
-  }
-
-  function getTotalRemaining (orders) {
-    return orders.filter(function (order) {
-      return order.paymentsPlan.status === 'failed' || order.paymentsPlan.status === 'pending'
-    }).reduce(function (previousPrice, order) {
-      return previousPrice + order.paymentsPlan.price
-    }, 0)
-  }
-
-  /* function getOrdersGroupByProductId (orders) {
-    return orders.reduce(function (prev, order, index, array) {
-      // console.log('index', index)
-      if (Object.keys(prev).indexOf(order.paymentsPlan.productInfo.productId) === -1) {
-        // prev.push({[order.paymentsPlan.productInfo.productId]: order})
-        prev[order.paymentsPlan.productInfo.productId] = []
-      }
-      prev[order.paymentsPlan.productInfo.productId].push(order)
-      return prev
-    }, []).filter(function (order) {
-      // console.log('order', order)
-      return !!order
-    })
-  }*/
-
-  /* function getTotalPriceProduct (orders) {
-    return orders.map(function (value) {
-      console.log('one value', value)
-      return value.reduce(function (sum, pp) {
-        console.log('one pp', pp)
-        return sum + pp.paymentsPlan.price
-      }, 0)
-    }, 0)
-  }*/
-
-  /* function getDiscountProduct (orders) {
-    return orders.map(function (value) {
-      console.log('one value', value)
-      return value.reduce(function (sum, pp) {
-        console.log('one pp', pp)
-        return sum + pp.paymentsPlan.discount
-      }, 0)
-    }, 0)
-  }*/
-
-  /* function getPaidProduct (orders) {
-    return orders.map(function (order) {
-      console.log('getPaidProduct order', order)
-      return order.paymentsPlan.status === 'succeeded'
-    }).reduce(function (previousPrice, order) {
-      return previousPrice + order.paymentsPlan.price
-    }, 0)
-  }*/
-
-  /* function getRemainingProduct (orders) {
-    return orders.filter(function (order) {
-      return order.paymentsPlan.status === 'failed' || order.paymentsPlan.status === 'pending'
-    }).reduce(function (previousPrice, order) {
-      return previousPrice + order.paymentsPlan.price
-    }, 0)
-  }*/
 }]
