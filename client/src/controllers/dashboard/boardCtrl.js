1a4321fda4d2727e698a93c1ac3fe9383eba7807
Jesse Cogollo <cogollo87@gmail.com>
2016-05-20 14:40:31 -0500

    Dashboard refactor complete

--- client/src/controllers/dashboard/boardCtrl.js
+++ client/src/controllers/dashboard/boardCtrl.js
@@ -1,45 +1,16 @@
 'use strict'
 
 module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', function ($scope, AuthService, $state, CommerceService) {
-  $scope.expandCategory1 = true
-  $scope.expandSection11 = false
-  $scope.totalPrice = 0
-  $scope.totalDiscount = 0
-  $scope.totalPaid = 0
-  $scope.totalRemaining = 0
-  $scope.groupProducts = []
-  AuthService.getCurrentUserPromise().then(function (user) {
-    CommerceService.orderGetOrganization(user._id, 20, -1).then(function (result) {
-      // console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
-      CommerceService.orderGetOrganization(user.meta.productRelated[0], 200, -1).then(function (result) {
-        $scope.totalPrice = getTotalPrice(result.body)
-        $scope.totalDiscount = getTotalDiscount(result.body)
-        $scope.totalPaid = getTotalPaid(result.body)
-        $scope.totalRemaining = getTotalRemaining(result.body)
-        var finalResult = R.groupBy(function (order) {
-          return order.allProductName[0]
-        })
-        $scope.groupProducts = finalResult(result.body)
-        // var temp = finalResult(result.body)
-        // console.log('temp', temp)
-        // console.log('result', result.body)
-      }).catch(function (err) {
-        console.log('err', err)
-      })
-    }).catch(function (err) {
-      console.log('err', err)
-    })
-  })
 
-  $scope.getSubTotal = function getSubTotal (subTotals) {
-    return subTotals.reduce(function (result, sTotals) {
-      return result + sTotals.sumoriginalPrice
+  $scope.getSubTotal = function getSubTotals (orders) {
+    return orders.reduce(function (result, order) {
+      return result + order.sumoriginalPrice
     }, 0)
   }
 
-  $scope.getSubTotalDiscount = function getSubTotalDiscount (subTotals) {
-    return subTotals.reduce(function (result, sTotals) {
-      return result + sTotals.sumDiscount
+  $scope.getSubTotalDiscount = function getSubTotalDiscount (orders) {
+    return orders.reduce(function (result, order) {
+      return result + order.sumDiscount
     }, 0)
   }
 
@@ -61,47 +32,34 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
     }, 0)
   }
 
-  function getTotalPrice (orders) {
-    return orders.reduce(function (previousPrice, order) {
-      return previousPrice + order.sumoriginalPrice
-    }, 0)
+  function init () {
+    $scope.expandCategory1 = true
+    $scope.expandSection11 = false
+    $scope.totalPrice = 0
+    $scope.totalDiscount = 0
+    $scope.totalPaid = 0
+    $scope.totalRemaining = 0
+    $scope.groupProducts = []
+    AuthService.getCurrentUserPromise().then(function (user) {
+      CommerceService.orderGetOrganization(user._id, 20, -1).then(function (result) {
+        // console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
+        CommerceService.orderGetOrganization(user.meta.productRelated[0], 200, -1).then(function (result) {
+          $scope.totalPrice = $scope.getSubTotal(result.body)
+          $scope.totalDiscount = $scope.getSubTotalDiscount(result.body)
+          $scope.totalPaid = $scope.getSubTotalPaid(result.body)
+          $scope.totalRemaining = $scope.getSubTotalRemaining(result.body)
+          var finalResult = R.groupBy(function (order) {
+            return order.allProductName[0]
+          })
+          $scope.groupProducts = finalResult(result.body)
+        }).catch(function (err) {
+          console.log('err', err)
+        })
+      }).catch(function (err) {
+        console.log('err', err)
+      })
+    })
   }
 
-  function getTotalDiscount (orders) {
-    return orders.reduce(function (previousPrice, order) {
-      return previousPrice + order.sumDiscount
-    }, 0)
-  }
-
-  function getTotalPaid (orders) {
-    return orders.reduce(function (total, order) {
-      return total + order.paymentsPlan.reduce(function (previousPrice, pp) {
-        var sum = (pp.status === 'succeeded') ? pp.originalPrice : 0
-        return previousPrice + sum
-      }, 0)
-    }, 0)
-  }
-
-  function getTotalRemaining (orders) {
-    return orders.reduce(function (total, order) {
-      return total + order.paymentsPlan.reduce(function (previousPrice, pp) {
-        var sum = (pp.status === 'failed' || pp.status === 'pending') ? pp.originalPrice : 0
-        return previousPrice + sum
-      }, 0)
-    }, 0)
-  }
-
-  $scope.getSubTotalPaidOrder = function getSubTotalPaidOrder (order) {
-    return order.paymentsPlan.reduce(function (previousPrice, pp) {
-      var sum = (pp.status === 'succeeded') ? pp.originalPrice : 0
-      return previousPrice + sum
-    }, 0)
-  }
-
-  $scope.getSubTotalRemainingOrder = function getSubTotalPaidOrder (order) {
-    return order.paymentsPlan.reduce(function (previousPrice, pp) {
-      var sum = (pp.status === 'failed' || pp.status === 'pending') ? pp.originalPrice : 0
-      return previousPrice + sum
-    }, 0)
-  }
+  init()
 }]
