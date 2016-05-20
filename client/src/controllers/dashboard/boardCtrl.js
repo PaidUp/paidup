ec43234fb41bd6cb31f2668c5b84d23797af1541
Jesse Cogollo <cogollo87@gmail.com>
2016-05-20 04:28:26 -0500

    dashboard coach, Done (first step)

--- client/src/controllers/dashboard/boardCtrl.js
+++ client/src/controllers/dashboard/boardCtrl.js
@@ -2,80 +2,27 @@
 
 module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', function ($scope, AuthService, $state, CommerceService) {
   $scope.expandCategory1 = true
-  $scope.expandSection11 = true
-  $scope.groupProducts = {
-    '14U': {
-      productName: '14U',
-      total: 2000,
-      discount: 0,
-      paid: 1000,
-      remaining: 1000,
-      orders: [{
-        createAt: '2016-05-03T06:02:12.692Z',
-        productName: 'NY Yankess',
-        beneficiaryName: 'Edgar Renteria',
-        total: '1000',
-        discount: '0',
-        paid: '1000',
-        remaining: '0',
-        orderId: '0001',
-        status: '0001',
-        pp: [{
-          dateCharge: '2016-05-03T06:02:12.692Z',
-          description: 'description',
-          brand: 'visa',
-          last4: '1234',
-          originalPrice: 950,
-          price: 1000,
-          status: 'pending'
-        },
-        {
-          dateCharge: '2016-05-03T06:02:12.692Z',
-          description: 'ok',
-          brand: 'visa',
-          last4: '1234',
-          originalPrice: 950,
-          price: 1000,
-          status: 'failed'
-        }]
-      }]
-    },
-    '15U': {
-      productName: '15U',
-      total: 4000,
-      discount: 0,
-      paid: 2000,
-      remaining: 2000,
-      orders: []
-    },
-    '16U': {
-      productName: '16U',
-      total: 2000,
-      discount: 0,
-      paid: 1500,
-      remaining: 500,
-      orders: []
-    },
-    '17U': {
-      productName: '17U',
-      total: 2000,
-      discount: 0,
-      paid: 1000,
-      remaining: 1000,
-      orders: []
-    }
-  }
+  $scope.expandSection11 = false
+  $scope.totalPrice = 0
+  $scope.totalDiscount = 0
+  $scope.totalPaid = 0
+  $scope.totalRemaining = 0
+  $scope.groupProducts = []
   AuthService.getCurrentUserPromise().then(function (user) {
     CommerceService.orderGetOrganization(user._id, 20, -1).then(function (result) {
-      console.log('result', result.body.orders)
-      // $scope.allOrdersBoard = result.body.orders
-      console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
-      CommerceService.orderGetOrganization('acct_18AQWDGKajSrnujf', 200, -1).then(function (result) {
-        console.log('result', result.body)
-        result.body.map(function (order) {
-          console.log('order', order)
-          return order
+      // console.log('user', user.meta.productRelated)// acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
+      CommerceService.orderGetOrganization(user.meta.productRelated[0], 200, -1).then(function (result) {
+        $scope.totalPrice = getTotalPrice(result.body)
+        $scope.totalDiscount = getTotalDiscount(result.body)
+        $scope.totalPaid = getTotalPaid(result.body)
+        $scope.totalRemaining = getTotalRemaining(result.body)
+        var finalResult = R.groupBy(function (order) {
+          return order.allProductName[0]
         })
+        $scope.groupProducts = finalResult(result.body)
+        // var temp = finalResult(result.body)
+        // console.log('temp', temp)
+        // console.log('result', result.body)
       }).catch(function (err) {
         console.log('err', err)
       })
@@ -83,4 +30,78 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
       console.log('err', err)
     })
   })
+
+  $scope.getSubTotal = function getSubTotal (subTotals) {
+    return subTotals.reduce(function (result, sTotals) {
+      return result + sTotals.sumoriginalPrice
+    }, 0)
+  }
+
+  $scope.getSubTotalDiscount = function getSubTotalDiscount (subTotals) {
+    return subTotals.reduce(function (result, sTotals) {
+      return result + sTotals.sumDiscount
+    }, 0)
+  }
+
+  $scope.getSubTotalPaid = function getSubTotalPaid (orders) {
+    return orders.reduce(function (result, order) {
+      return result + order.paymentsPlan.reduce(function (previousPrice, pp) {
+        var sum = (pp.status === 'succeeded') ? pp.originalPrice : 0
+        return previousPrice + sum
+      }, 0)
+    }, 0)
+  }
+
+  $scope.getSubTotalRemaining = function getSubTotalRemaining (subTotals) {
+    return subTotals.reduce(function (result, sTotals) {
+      return result + sTotals.paymentsPlan.reduce(function (previousPrice, pp) {
+        var sum = (pp.status === 'failed' || pp.status === 'pending') ? pp.originalPrice : 0
+        return previousPrice + sum
+      }, 0)
+    }, 0)
+  }
+
+  function getTotalPrice (orders) {
+    return orders.reduce(function (previousPrice, order) {
+      return previousPrice + order.sumoriginalPrice
+    }, 0)
+  }
+
+  function getTotalDiscount (orders) {
+    return orders.reduce(function (previousPrice, order) {
+      return previousPrice + order.sumDiscount
+    }, 0)
+  }
+
+  function getTotalPaid (orders) {
+    return orders.reduce(function (total, order) {
+      return total + order.paymentsPlan.reduce(function (previousPrice, pp) {
+        var sum = (pp.status === 'succeeded') ? pp.originalPrice : 0
+        return previousPrice + sum
+      }, 0)
+    }, 0)
+  }
+
+  function getTotalRemaining (orders) {
+    return orders.reduce(function (total, order) {
+      return total + order.paymentsPlan.reduce(function (previousPrice, pp) {
+        var sum = (pp.status === 'failed' || pp.status === 'pending') ? pp.originalPrice : 0
+        return previousPrice + sum
+      }, 0)
+    }, 0)
+  }
+
+  $scope.getSubTotalPaidOrder = function getSubTotalPaidOrder (order) {
+    return order.paymentsPlan.reduce(function (previousPrice, pp) {
+      var sum = (pp.status === 'succeeded') ? pp.originalPrice : 0
+      return previousPrice + sum
+    }, 0)
+  }
+
+  $scope.getSubTotalRemainingOrder = function getSubTotalPaidOrder (order) {
+    return order.paymentsPlan.reduce(function (previousPrice, pp) {
+      var sum = (pp.status === 'failed' || pp.status === 'pending') ? pp.originalPrice : 0
+      return previousPrice + sum
+    }, 0)
+  }
 }]
