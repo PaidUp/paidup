9b034569a08494595ee201cc7162fda5128abaae
Ricardo Lara <riclara@gmail.com>
2016-05-23 16:53:13 -0500

    PU-15 Filter inactive categories

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,8 +1,16 @@
 'use strict'
 // var angular = require('angular')
 
-module.exports = ['$resource', '$q', function ($resource, $q) {
+module.exports = ['$resource', '$q', '$cookies', function ($resource, $q, $cookies) {
   var getCategories = $resource('/api/v1/commerce/catalog/categories', {}, {})
+
+  this.getPnProducts = function(){
+    return $cookies.get('pnProds')
+  }
+
+  this.setPnProducts = function(pnProds){
+    $cookies.set('pnProds', pnProds)
+  }
 
   this.retrieveCategories = function () {
     return getCategories.get({}).$promise;
