1c984e87ecb6ff83c227bc176ceae80af6c89b27
Jesse Cogollo <cogollo87@gmail.com>
2016-05-24 15:11:52 -0500

    start process to deposits dasboard

--- client/src/controllers/dashboard/boardCtrl.js
+++ client/src/controllers/dashboard/boardCtrl.js
@@ -34,6 +34,71 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
     }, 0)
   }
 
+  $scope.getCVS = function getCVS () {
+    downloadCSV({})
+  }
+
+  function convertArrayOfObjectsToCSV (args) {
+    // http://halistechnology.com/2015/05/28/use-javascript-to-export-your-data-as-csv/
+    // http://csv.adaltas.com/generate/
+    // https://github.com/voodootikigod/node-csv
+    // https://github.com/koles/ya-csv
+    // https://www.npmjs.com/package/csv
+
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data
+
+    data = args.data || null
+    if (data == null || !data.length) {
+      return null
+    }
+
+    columnDelimiter = args.columnDelimiter || ','
+    lineDelimiter = args.lineDelimiter || '\n'
+
+    keys = Object.keys(data[0])
+
+    result = ''
+    result += keys.join(columnDelimiter)
+    result += lineDelimiter
+    console.log('keys', keys)
+    data.forEach(function (item) {
+      console.log('item', item)
+      ctr = 0
+      keys.forEach(function (key) {
+        console.log('key', key)
+        console.log('key', key.length)
+        if (ctr > 0) result += columnDelimiter
+
+        result += item[key]
+        ctr++
+      })
+      result += lineDelimiter
+    })
+    console.log('result', result)
+    return result
+  }
+
+  function downloadCSV (args) {
+    var data, filename, link
+    var csv = convertArrayOfObjectsToCSV({
+      data: [$scope.groupProducts]
+    })
+    console.log('csv', csv)
+    if (csv == null) return
+
+    filename = args.filename || 'export.csv'
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = 'data:text/csv;charset=utf-8,' + csv
+    }
+    data = encodeURI(csv)
+
+    // link = document.createElement('a')
+    // link.setAttribute('href', data)
+    // link.setAttribute('download', filename)
+    // link.click()
+  }
+
   function init () {
     $scope.expandCategory1 = true
     $scope.expandSection11 = false
@@ -47,7 +112,7 @@ module.exports = [ '$scope', 'AuthService', '$state', 'CommerceService', functio
     $scope.groupProducts = []
     AuthService.getCurrentUserPromise().then(function (user) {
       // user.meta.productRelated[0]) - acct_17vBpJHXmwMXUx1q - acct_18AQWDGKajSrnujf
-      let organizationId = (user.meta.productRelated[0]) ? user.meta.productRelated[0] : 'Does not have organization'
+      var organizationId = (user.meta.productRelated[0]) ? user.meta.productRelated[0] : 'Does not have organization'
       CommerceService.orderGetOrganization(organizationId, 200, -1).then(function (result) {
         $scope.totalPrice = $scope.getSubTotal(result.body)
         $scope.totalPriceFees = $scope.getSubTotal(result.body, 'sumPrice')
