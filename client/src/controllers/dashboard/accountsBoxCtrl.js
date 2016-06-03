20c300bad14d5440d737c22298c9c28491ce212f
Jesse Cogollo <cogollo87@gmail.com>
2016-06-03 17:03:49 -0500

    PU-46 Done

--- client/src/controllers/dashboard/accountsBoxCtrl.js
+++ client/src/controllers/dashboard/accountsBoxCtrl.js
@@ -3,26 +3,26 @@
 module.exports = [ '$scope', '$rootScope', '$state', 'PaymentService', 'AuthService', function ($scope, $rootScope, $state, PaymentService, AuthService) {
 
   $rootScope.$on('reloadAccountsBox', function (event, data) {
-    $scope.init();
+    $scope.init()
   })
 
   $scope.payments = []
 
-  $scope.init = function(){
+  $scope.init = function () {
     AuthService.getCurrentUserPromise().then(function (user) {
       PaymentService.listCards(user._id).then(function (cards) {
         $scope.payments = cards.data
+        cards.data.length ? $rootScope.$emit('existsAccount', true) : $rootScope.$emit('existsAccount', false)
       }).catch(function (err) {
         console.log('ERR', err)
       })
     }).catch(function (err) {
       console.log('err', err)
     })
-
   }
 
-  $scope.getBrandCardClass = function(brand){
-    return PaymentService.getBrandCardClass(brand);
+  $scope.getBrandCardClass = function (brand) {
+    return PaymentService.getBrandCardClass(brand)
   }
 
   $scope.updateAccounts = function () {
