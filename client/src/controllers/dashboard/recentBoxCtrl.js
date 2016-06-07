3ac9eb83194af907c31effa10ed22c4f78be02b7
Jesse Cogollo <cogollo87@gmail.com>
2016-06-07 11:38:02 -0500

    Update PU-6 the recent box always is visible

--- client/src/controllers/dashboard/recentBoxCtrl.js
+++ client/src/controllers/dashboard/recentBoxCtrl.js
@@ -1,14 +1,10 @@
 'use strict'
 
-module.exports = [ '$rootScope', '$scope', 'AuthService', '$state', 'CommerceService', function ($rootScope, $scope, AuthService, $state, CommerceService) {
+module.exports = ['$scope', 'AuthService', '$state', 'CommerceService', function ($scope, AuthService, $state, CommerceService) {
   $scope.orders = []
-  $scope.isVisible = false
   AuthService.getCurrentUserPromise().then(function (user) {
     CommerceService.getRecentOrders(user._id, 10).then(function (result) {
       $scope.orders = result.body
-      $rootScope.$on('existsAccount', function (event, value) {
-        $scope.isVisible = value
-      })
     }).catch(function (err) {
       console.log('err', err)
     })
