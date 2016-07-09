8ef7b6c103f810931e11ad7567ba26c967666210
Ricardo Lara <riclara@gmail.com>
2016-07-08 21:56:52 -0500

    startPU-9 Implement new PaidUp Admin Site.  update authentication url

--- client/src/controllers/dashboard/headerCtrl.js
+++ client/src/controllers/dashboard/headerCtrl.js
@@ -14,13 +14,13 @@ module.exports = [ '$scope', '$rootScope', '$window', '$location', 'SessionServi
     var token = SessionService.getCurrentSession();
 
     if(host.indexOf('app') === 0){
-      adminUrl = 'https://admin.getpaidup.com/token/'+token;
+      adminUrl = 'https://admin.getpaidup.com/access?token='+token;
     }
     else if(host.indexOf('stg') === 0){
-      adminUrl = 'https://admstg.getpaidup.com/token/'+token;
+      adminUrl = 'https://admstg.getpaidup.com/access?token='+token;
     }
     else if(host.indexOf('dev') === 0){
-      adminUrl = 'https://admdev.getpaidup.com/token/'+token;
+      adminUrl = 'https://admdev.getpaidup.com/access?token='+token;
     }
     else {
       adminUrl = 'http://localhost:4000/access?token='+token;
