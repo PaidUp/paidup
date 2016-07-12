176bb37037008a47a1815b533cbb8a8974ca823c
Ricardo Lara <riclara@gmail.com>
2016-07-12 11:32:59 -0500

    PU-169 Cannot Advance After Selecting Team

--- client/src/services/setupPaymentService.js
+++ client/src/services/setupPaymentService.js
@@ -1,7 +1,7 @@
 'use strict'
 // var angular = require('angular')
 
-module.exports = [ '$q', 'localStorageService', function ( $q, localStorageService) {
+module.exports = [function () {
 
   var service = this;
 
