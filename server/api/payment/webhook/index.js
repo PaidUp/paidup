0dcc671e49035c08ea69060142c7380cdf9c3de6
Jesse Cogollo <cogollo87@gmail.com>
2016-07-21 15:58:38 -0500

    webhook test

--- server/api/payment/webhook/index.js
+++ server/api/payment/webhook/index.js
@@ -8,4 +8,16 @@ router.post('/', controller.webpost)
 router.get('/', controller.webget)
 router.post('/charge', controller.webgetpaymentcharge)
 
+router.post('/charge/account/captured', controller.captured)
+router.post('/charge/account/failed', controller.failed)
+router.post('/charge/account/refunded', controller.refunded)
+router.post('/charge/account/succeeded', controller.succeeded)
+router.post('/charge/account/updated', controller.updated)
+
+router.post('/charge/cc/captured', controller.ccaptured)
+router.post('/charge/cc/failed', controller.cfailed)
+router.post('/charge/cc/refunded', controller.crefunded)
+router.post('/charge/cc/succeeded', controller.csucceeded)
+router.post('/charge/cc/updated', controller.cupdated)
+
 module.exports = router
--- server/api/payment/webhook/webhook.controller.js
+++ server/api/payment/webhook/webhook.controller.js
@@ -15,7 +15,68 @@ exports.webget = function (req, res) {
 }
 
 exports.webgetpaymentcharge = function (req, res) {
+  console.log('req.body', req.body)
   webhookService.sendEmail(req.body, '[Charge]', function (err, data) {
     return res.status(200).json({webhook: 'TODO verifyAndUpdateOrdersWithBankAccounts'})
   })
+}
+
+exports.captured = function (req, res) {
+  console.log('captured')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'captured'})
+}
+
+exports.failed = function (req, res) {
+  console.log('failed')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'failed'})
+}
+
+exports.refunded = function (req, res) {
+  console.log('refunded')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'refunded'})
+}
+
+exports.succeeded = function (req, res) {
+  console.log('succeeded')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'succeeded'})
+}
+
+exports.updated = function (req, res) {
+  console.log('updated')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'updated'})
+}
+
+exports.ccaptured = function (req, res) {
+  console.log('ccaptured')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'ccaptured'})
+}
+
+exports.cfailed = function (req, res) {
+  console.log('cfailed')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'cfailed'})
+}
+
+exports.crefunded = function (req, res) {
+  console.log('crefunded')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'crefunded'})
+}
+
+exports.csucceeded = function (req, res) {
+  console.log('csucceeded')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'csucceeded'})
+}
+
+exports.cupdated = function (req, res) {
+  console.log('cupdated')
+  console.log('req.body', req.body)
+  return res.status(200).json({webhook: 'cupdated'})
 }
