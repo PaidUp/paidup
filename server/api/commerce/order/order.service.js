fbae549afdbb5087a4a53da0c01efb8e56069510
Jesse Cogollo <cogollo87@gmail.com>
2016-07-22 17:59:39 -0500

    add console

--- server/api/commerce/order/order.service.js
+++ server/api/commerce/order/order.service.js
@@ -504,6 +504,7 @@ function orderUpdateWebhook (data, cb) {
     },
     // OK.
     success: function (result) {
+      console.log('result service', result)
       result.body = JSON.parse(result.body)
       return cb(null, result)
     }
