ad596995e6e01278c857adc2806091ab2b50af1f
Ricardo Lara <riclara@gmail.com>
2016-10-19 09:50:18 -0500

    PU-266

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,26 +1,48 @@
 'use strict'
 // var angular = require('angular')
 
-module.exports = ['$resource', '$q', '$cookies', function ($resource, $q, $cookies) {
+module.exports = ['$resource', '$q', '$cookies', '$rootScope', 'UserService', function ($resource, $q, $cookies, $rootScope, UserService) {
+  var ProductService = this;  
   var getCategories = $resource('/api/v1/commerce/catalog/categories', {}, {})
 
-  this.getPnProducts = function(){
-    var pn = $cookies.get('pnProds') ? JSON.parse($cookies.get('pnProds')) : {};
+
+  ProductService.getPnProducts = function (cb) {
+    var currentUser = $rootScope.currentUser;
+
+    if ($cookies.get('pnProds')) {
+      var pn = $cookies.get('pnProds') ? JSON.parse($cookies.get('pnProds')) : {};
+      var pSuggested = currentUser.meta.productSuggested;
+      var merge = Object.assign(pSuggested, pn);
+      UserService.updateProductsSuggested(currentUser._id, merge).then(function(newUser){
+        $rootScope.currentUser = newUser; 
+        ProductService.removePnProducts();
+        cb(null, newUser.meta.productSuggested);
+      }).catch(function(err){
+        cb(err);
+      })
+
+
+    }
+
+
+    if (currentUser.meta.productSuggested) {
+
+    }
 
     return pn
   }
 
-  this.categories = [];
+  ProductService.categories = [];
 
-  this.setPnProducts = function(pnProds){
+  ProductService.setPnProducts = function (pnProds) {
     $cookies.put('pnProds', JSON.stringify(pnProds))
   }
 
-  this.removePnProducts = function(pnProds){
+  ProductService.removePnProducts = function (pnProds) {
     $cookies.remove('pnProds')
   }
 
-  this.retrieveCategories = function () {
+  ProductService.retrieveCategories = function () {
     return getCategories.get({}).$promise;
   }
 }]
