d4c2b5d8f2144ddb80618d040dfaf3e69dd666dd
Ricardo Lara <riclara@gmail.com>
2016-10-28 09:53:21 -0500

    Pu-269

--- client/src/controllers/dashboard/dashboardCtrl.js
+++ client/src/controllers/dashboard/dashboardCtrl.js
@@ -3,14 +3,15 @@
 module.exports = [ '$scope', 'AuthService', '$state', 'SessionService', function ($scope, AuthService, $state, SessionService) {
   $scope.PageOptions.pageClass = 'dashboard-page'
 
-  $scope.isReferring = SessionService.getReferringDomain() ? true : false;
+  //$scope.isReferring = SessionService.getReferringDomain() ? true : false;
   $scope.refDomain = SessionService.getReferringDomain();
   $scope.refLogo = SessionService.getReferringLogo();
 
   AuthService.getCurrentUserPromise().then(function (u) {
     $scope.user = u
-    $scope.isCoach = AuthService.isCoach()
+    $scope.isCoach = AuthService.isCoach();
     $scope.isAdmin = AuthService.isAdmin();
+    $scope.isReferring = u.meta.referrer && u.meta.referrer.length > 0;
   }, function (e) {
     console.log(e)
   })
